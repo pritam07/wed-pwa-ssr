0a206e51d96a6418b1cb7b2ea1133b7f98a04466
pritam07 <pritam07@users.noreply.github.com>
2018-05-22 21:25:07 +0530

    Update shared.module.ts

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,20 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatMenuModule, MatIconModule, MatBadgeModule} from '@angular/material';
 import { NgxsModule } from '@ngxs/store';
 import { TutorialState } from '../../state/tutorial.state';
 
 @NgModule({
   imports: [
     CommonModule,
-    [MatButtonModule, MatCheckboxModule]
+    [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatMenuModule, MatIconModule, MatBadgeModule]
   ],
   declarations: [],
   exports: [
     CommonModule,   
     MatButtonModule,
     MatCheckboxModule,
-    
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatBadgeModule   
   ]
 })
 export class SharedModule {}
