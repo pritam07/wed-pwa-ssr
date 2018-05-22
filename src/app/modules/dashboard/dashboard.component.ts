bb8ab85a026e9db522e344bee6ecfbdfda2828a4
pritam07 <pritam07@users.noreply.github.com>
2018-05-22 17:46:44 +0530

    Update dashboard.component.ts

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store, Select } from '@ngxs/store';
+import { Tutorial } from '../../models/tutorial.model'
+import { TutorialState } from '../../state/tutorial.state' // We will use this shortly
+import { Observable } from 'rxjs'
+import { RemoveTutorial } from '../../actions/tutorial.actions'
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,7 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  tutorials$: Observable<Tutorial>
+
+  constructor(private store: Store) {
+        this.tutorials$ = this.store.select(state => state.tutorials.tutorials)
+  }
+  delTutorial(name) {
+    this.store.dispatch(new RemoveTutorial(name))
+  }
 
   ngOnInit() {
   }
