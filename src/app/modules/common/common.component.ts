2c923cd7cab190f5f75d61973b64b44298c9afa3
pritam07 <pritam07@users.noreply.github.com>
2018-06-04 13:16:43 +0530

    Update common.component.ts

--- src/app/modules/common/common.component.ts
+++ src/app/modules/common/common.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef} from '@angular/core';
 import { Store } from '@ngxs/store';
 import { AddTutorial } from '../.././actions/tutorial.actions';
 import {MediaMatcher} from '@angular/cdk/layout';
