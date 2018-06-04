972f702e7ab7134efd46756e6699de41fab593ef
pritam07 <pritam07@users.noreply.github.com>
2018-06-04 20:40:18 +0530

    Update dashboard.component.ts

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { Tutorial } from '../../models/tutorial.model'
 import { TutorialState } from '../../state/tutorial.state' // We will use this shortly
 import { Observable } from 'rxjs'
 import { RemoveTutorial } from '../../actions/tutorial.actions'
+import { FormArray, FormBuilder, FormGroup, Validators  } from '@angular/forms'
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -11,16 +12,27 @@ import { RemoveTutorial } from '../../actions/tutorial.actions'
 })
 export class DashboardComponent implements OnInit {
 
+  signupForm: FormGroup;
   tutorials$: Observable<Tutorial>
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private fb: FormBuilder,) {
         this.tutorials$ = this.store.select(state => state.tutorials.tutorials)
   }
   delTutorial(name) {
     this.store.dispatch(new RemoveTutorial(name))
   }
-
+createSignupForm() {
+    this.signupForm = this.fb.group({
+    name: ['', Validators.required],    
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+}
   ngOnInit() {
+    this.createSignupForm()
   }
+  onSubmit() {
 
+  }
+  get f() { return this.signupForm.controls; }
 }
